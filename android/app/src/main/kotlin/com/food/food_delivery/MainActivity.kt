f0c3881a27ad46bf32a671bdfeac27871da87376
Sauvik Nath <sauviknath10@gmail.com>
2020-12-02 11:07:38 +0530

    Built web

--- android/app/src/main/kotlin/com/food/food_delivery/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.food.food_delivery
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -13,8 +13,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "2c40a93e6deb643cbd3b7b5aea5e07ea",
-"/": "2c40a93e6deb643cbd3b7b5aea5e07ea",
+"index.html": "dcbe797eee28b860b1b96fe27d3c5bf6",
+"/": "dcbe797eee28b860b1b96fe27d3c5bf6",
 "main.dart.js": "6603a0bf9502aaebc8f2e13c64175aa9",
 "manifest.json": "8a7044b7bec4e9d222f5d5937d73e545"
 };
